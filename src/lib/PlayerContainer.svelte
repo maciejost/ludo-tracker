52e7b7b35555b12ed386db6644a5f0df3a3d70d9
Maciej Ostrowski <ostromaciej@gmail.com>
2023-12-27 18:16:23 +0100

    Legg til spillerlogikk

---
+++ src/lib/PlayerContainer.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	export let playerColor: Player
+	export let game: Game
+	export let updateGame: (player: Player, powerUp: keyof PowerUps, value: number) => void
+</script>
+<div>
+	<h2>
+		{playerColor}
+	</h2>
+	<div>
+		<p>Double throw</p>
+		<p>{game[playerColor].double}</p>
+		<button
+			on:click={() => updateGame(playerColor, 'double', game[playerColor].double + 1)}
+		>
+			+1
+		</button>
+		<button
+			disabled={game[playerColor].double === 0}
+			on:click={() => updateGame(playerColor, 'double', game[playerColor].double - 1)}
+		>
+			-1
+		</button>
+	</div>
+	<div>
+		<p>Choose throw</p>
+		<p>{game[playerColor].choose}</p>
+		<button
+
+			on:click={() => updateGame(playerColor, 'choose', game[playerColor].choose + 1)}
+		>
+			+1
+		</button>
+		<button
+			disabled={game[playerColor].choose === 0}
+			on:click={() => updateGame(playerColor, 'choose', game[playerColor].choose - 1)}
+		>
+			-1
+		</button>
+	</div>
+	<div>
+		<p>Shield</p>
+		<p>{game[playerColor].shield}</p>
+		<button
+			on:click={() => updateGame(playerColor, 'shield', game[playerColor].shield + 1)}
+		>
+			+1
+		</button>
+		<button
+		disabled={game[playerColor].shield === 0}
+			on:click={() => updateGame(playerColor, 'shield', game[playerColor].shield - 1)}
+		>
+			-1
+		</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,21 @@
 
+
 <script lang="ts">
 	export const prerender = true
 
-	type PowerUps = {
+	import PlayerContainer from '$lib/PlayerContainer.svelte'
+
+	export type PowerUps = {
 	[key: 'shield' | 'choose' | 'double'] : number
 	}
 
-	type Player = 'green' | 'red' | 'blue' | 'yellow'
+	export type Player = 'green' | 'red' | 'blue' | 'yellow'
 
 	type Game = {
 		[key in Player]: PowerUps
 	}
 
-	let game = {
+	const DEFAULT_GAME = {
 		green: {
 			shield: 0,
 			choose: 0,
@@ -35,8 +38,32 @@
 		}
 	}
 
+	let game = JSON.parse(JSON.stringify(DEFAULT_GAME)) as Game
+
+	const updateGame = (player: Player, powerUp: keyof PowerUps, value: number) => {
+
+		if (value < 0) {
+			value = 0
+		}
+
+		game = {
+			...game,
+			[player]: {
+				...game[player],
+				[powerUp]: value
+			}
+		}
+
+		console.log('game', game)
+	}
+
+	const players = ['green', 'red', 'blue', 'yellow']
+
 </script>
 <main>
 	<h1>Ludo World Tracker</h1>
 	<p>A companion app for <a href="https://www.facebook.com/RealLudoSuperstar/" target="_blank">Ludo World</a> to help you keep track of your opponents powerups.</p>
+	{#each players as player}
+		<PlayerContainer playerColor={player} {game} {updateGame} />
+	{/each}
 </main>
